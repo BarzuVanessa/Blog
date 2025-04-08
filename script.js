97854d2594ffaa63938445c71b7ba2c0313d06e5
BarzuVanessa <barzuvanessa@gmail.com>
2025-04-08 23:00:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openALLARTICLES () {
+    window.location.href= 'index.html';
+ }
+ 
+ 
+ function openCONTACT(){
+     window.location.href ='contact.html';
+ }
+ 
+ 
+ function openREVIEWS () {
+    window.location.href='reviews.html';
+ }
